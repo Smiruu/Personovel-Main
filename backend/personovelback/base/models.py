19af8cf316b146adfbd801b362cd9f76c5e62583
Lanz Elijah P. Linag <lanzlinag@gmail.com>
2024-02-18 00:10:11 +0800

    author and instance book

--- backend/personovelback/base/models.py
+++ backend/personovelback/base/models.py
@@ -11,8 +11,8 @@ def get_filename_ext(filepath):
 def upload_image_path(instance, filename):
     new_filename = random.randint(1, 3910209312)
     name, ext = get_filename_ext(filename)
-    final_filename = f"{new_filename}{ext}"
-    return f"static_my_project{new_filename}/{final_filename}"
+    final_filename = "{new_filename}{ext}".format(new_filename=new_filename, ext=ext)
+    return "img/{new_filename}/{final_filename}".format(new_filename=new_filename, final_filename=final_filename)
 # Create your models here.
 
 class Genre(models.Model):
