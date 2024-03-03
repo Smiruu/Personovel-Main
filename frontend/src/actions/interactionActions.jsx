d7175e9adb730c59c6ff270a756d3842b69407bc
Lanz Elijah P. Linag <lanzlinag@gmail.com>
2024-03-03 19:26:35 +0800

    detailed view chapters

--- frontend/src/actions/interactionActions.jsx
+++ frontend/src/actions/interactionActions.jsx
@@ -41,35 +41,33 @@ export const listInteractions = () => async (dispatch) => {
 
 export const listInteractionDetails = (id) => async (dispatch) => {
   try {
-    // Dispatch action to indicate the start of the request
     dispatch({ type: INTERACTION_DETAILS_REQUEST });
 
-    // Check if ID is valid
     if (!id && id !== 0) {
       throw new Error('Invalid Interaction ID');
     }
 
-    // Get user token from localStorage
     const userInfo = JSON.parse(localStorage.getItem('userInfo'));
     const token = userInfo ? userInfo.token.access : null;
 
-    // Configure Axios request headers with token
-    const config = {
+    const config = token ? {
       headers: {
-        'Authorization': token ? `Bearer ${token}` : '',
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': `Bearer ${token}`
       }
+    } : {};
+
+    const { data } = await instance.get(`/api/interactions/${id}`, config);
+    const updatedData = {
+      ...data,
     };
 
-    // Make GET request to fetch interaction details
-    const { data } = await instance.get(`/api/interactions/${id}`, config);
-
-    // Dispatch action with fetched data
     dispatch({
       type: INTERACTION_DETAILS_SUCCESS,
-      payload: data,
+      payload: updatedData,
     });
   } catch (error) {
-    // Dispatch action with error message
     dispatch({
       type: INTERACTION_DETAILS_FAIL,
       payload: error.response && error.response.data.detail
@@ -78,3 +76,4 @@ export const listInteractionDetails = (id) => async (dispatch) => {
     });
   }
 };
+  
